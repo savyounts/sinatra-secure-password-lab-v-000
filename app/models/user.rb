e001879f10f5281de422c747b2b0c5d946a7f5b7
Savannah Younts <savyounts@gmail.com>
2018-09-09 21:50:42 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :username, :presence => true
+
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  def withdrawl(amount)
+    self.balance -= amount if current_user.balance > amount
+  end
 end
