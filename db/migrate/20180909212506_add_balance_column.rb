1e16d10d39a92867562004bfe4d4e68de35d5410
Savannah Younts <savyounts@gmail.com>
2018-09-09 21:26:40 +0000

    automatically backed up by learn

--- db/migrate/20180909212506_add_balance_column.rb
+++ db/migrate/20180909212506_add_balance_column.rb
@@ -1,5 +1,5 @@
 class AddBalanceColumn < ActiveRecord::Migration
   def change
-    add_column :users, :balance, :integer, options={default: 0}
+    add_column :users, :balance, :integer, :default => 0
   end
 end
